8678312f56ee74dacca3d3ed3f7bcc16cf73ee1c
nekroment <adamcevs@gmail.com>
2020-10-11 03:39:25 +0300

    add components to App component

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Diagram from './components/Diagram';
+import Logs from './components/Logs';
+import Vote from './components/Vote';
+import { getLogsThunkCreator, getVotesThunkCreator, newVoteThunkCreator } from './redux/reducer/numberReducer';
 
-function App() {
+function App(props) {
+
+  async function getVotes(date) {
+    await props.getVotes(date);
+  }
+
+  useEffect(() => {
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    const date = day + '-' + month + '-' + year;
+    getVotes(date)}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/vote' component={() => <Vote newVote={props.newVote}/>} />
+        <Route path='/logs' component={() => <Logs logs={props.logs}/>} />
+        <Route path='/' component={() => <Diagram votes={props.votes} getVotes={props.getVotes}/>} />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  votes: state.vote.votes,
+  logs: state.vote.logs
+});
+
+export default connect(mapStateToProps, {
+  getVotes: getVotesThunkCreator,
+  getLogs: getLogsThunkCreator,
+  newVote: newVoteThunkCreator
+})(App);
